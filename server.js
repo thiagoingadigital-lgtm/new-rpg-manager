594eed4ecf825ea225c81a63ab6b25655fa7781e
Thiago Inga Digital <thiago@vallus.dev>
2026-08-19 14:48:54 +0000

    feat: unifica layout do site e define home como entrada

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const UPLOADS_DIR = path.join(__dirname, 'public', 'uploads');
 if (!fs.existsSync(UPLOADS_DIR)) fs.mkdirSync(UPLOADS_DIR, { recursive: true });
 
 app.use(express.json());
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public', 'home.html')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // ---------- Upload de imagens (retrato do personagem) ----------
